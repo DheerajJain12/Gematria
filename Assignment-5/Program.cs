824a3d07c3341967dc7b05370fd2b97e264ada03
visualstudio <visualstudio@122-PC14>
2019-04-12 18:17:53 -0400

    Code after debug

--- Assignment-5/Program.cs
+++ Assignment-5/Program.cs
@@ -8,13 +8,13 @@ namespace Assignment_5
 {
     class Program2
     {
-
         string[] Letters = new string[26] { "a", "b", "c", "d","e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r",
                                            "s", "t", "u", "v", "w", "x", "y", "z" };
         public static void Main()
         {
             Program2 orange = new Program2();
-            Console.WriteLine(orange.Gematria("Universe"));
+            Console.WriteLine(orange.Gematria("universe"));
+            Console.ReadLine();
         }
         public int Gematria(string word)
         {
@@ -25,18 +25,17 @@ namespace Assignment_5
             }
             return GematriaValue;
         }
-
         public int LetterValue(string letter)
         {
             int x = 0;
+
             while (Letters[x] != letter)
             {
                 if (Letters[x++] == letter)
-                {
-                    return x;
-                }
+                
+                    return -1;               
             }
-            return -1;
-        }
+            return x + 1;
+        }      
     }
 }
